14a651f7deb5f0f0ad394ba35433e740383d4e11
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 16:08:53 +0530

    Create two_pointers02.py

---
+++ two_pointers02.py
@@ -0,0 +1,41 @@
+'''
+Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
+
+Example 1:
+
+Input: [2, 3, 3, 3, 6, 9, 9]
+Output: 4
+Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+Example 2:
+
+Input: [2, 2, 2, 11]
+Output: 2
+Explanation: The first two elements after removing the duplicates will be [2, 11].
+
+Time Complexity #
+The time complexity of the above algorithm will be O(N)O(N), where ‘N’ is the total number of elements in the given array.
+
+Space Complexity #
+The algorithm runs in constant space O(1)O(1).
+
+'''
+def remove_duplicates(arr):
+  # index of the next non-duplicate element
+  next_non_duplicate = 1
+
+  i = 1
+  while(i < len(arr)):
+    if arr[next_non_duplicate - 1] != arr[i]:
+      arr[next_non_duplicate] = arr[i]
+      next_non_duplicate += 1
+    i += 1
+
+  return next_non_duplicate
+
+
+def main():
+  print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+  print(remove_duplicates([2, 2, 2, 11]))
+
+
+main()
