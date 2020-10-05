bc9dc192ca28da6346bbd44b9be4654d7bd751da
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 16:06:16 +0530

    Create two_pointers01.py

---
+++ two_pointers01.py
@@ -0,0 +1,31 @@
+'''
+Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+
+Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+Example 1:
+Input: [1, 2, 3, 4, 6], target=6
+Output: [1, 3]
+Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+
+Example 2:
+Input: [2, 5, 9, 11], target=11
+Output: [0, 2]
+Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11
+'''
+
+def pair_with_targetsum(arr, target_sum):
+  nums = {}  # to store numbers and their indices
+  for i, num in enumerate(arr):
+    if target_sum - num in nums:
+      return [nums[target_sum - num], i]
+    else:
+      nums[arr[i]] = i
+  return [-1, -1]
+
+
+def main():
+  print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
