849135c5d1bec309478dcdae74c6f93357f34128
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 16:07:44 +0530

    Update two_pointers01.py

--- two_pointers01.py
+++ two_pointers01.py
@@ -35,3 +35,32 @@ def main():
 
 
 main()
+
+''' Slightly slower approach using Binary Search Down Below
+Time Complexity #
+The time complexity of the above algorithm will be O(N)O(N), where ‘N’ is the total number of elements in the given array.
+
+Space Complexity #
+The algorithm runs in constant space O(1)O(1).
+'''
+
+def pair_with_targetsum(arr, target_sum):
+  left, right = 0, len(arr) - 1
+  while(left < right):
+    current_sum = arr[left] + arr[right]
+    if current_sum == target_sum:
+      return [left, right]
+
+    if target_sum > current_sum:
+      left += 1  # we need a pair with a bigger sum
+    else:
+      right -= 1  # we need a pair with a smaller sum
+  return [-1, -1]
+
+
+def main():
+  print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
