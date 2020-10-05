546c2b4bd09d2cc6b8a2bcb19107855400df2629
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 16:27:52 +0530

    Create fast_slow04.py

---
+++ fast_slow04.py
@@ -0,0 +1,55 @@
+'''
+Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+If the total number of nodes in the LinkedList is even, return the second middle node.
+
+Example 1:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> null
+Output: 3
+Example 2:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+Output: 4
+Example 3:
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
+Output: 4
+
+Time complexity #
+The above algorithm will have a time complexity of O(N)O(N) where ‘N’ is the number of nodes in the LinkedList.
+
+Space complexity #
+The algorithm runs in constant space O(1)O(1).
+'''
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+def find_middle_of_linked_list(head):
+  slow = head
+  fast = head
+  while (fast is not None and fast.next is not None):
+    slow = slow.next
+    fast = fast.next.next
+  return slow
+
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+
+  print("Middle Node: " + str(find_middle_of_linked_list(head).value))
+
+  head.next.next.next.next.next = Node(6)
+  print("Middle Node: " + str(find_middle_of_linked_list(head).value))
+
+  head.next.next.next.next.next.next = Node(7)
+  print("Middle Node: " + str(find_middle_of_linked_list(head).value))
+
+
+main()
