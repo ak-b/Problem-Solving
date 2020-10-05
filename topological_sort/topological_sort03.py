4883a3389b49e6eddc6b13e27020080adf3b2705
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 18:28:49 +0530

    Create topological_sort03.py

---
+++ topological_sort/topological_sort03.py
@@ -0,0 +1,77 @@
+'''
+There are ‘N’ tasks, labeled from ‘0’ to ‘N-1’. Each task can have some prerequisite tasks which need to be completed before it can be scheduled. Given the number of tasks and a list of prerequisite pairs, write a method to find the ordering of tasks we should pick to finish all tasks.
+
+Example 1:
+
+Input: Tasks=3, Prerequisites=[0, 1], [1, 2]
+Output: [0, 1, 2]
+Explanation: To execute task '1', task '0' needs to finish first. Similarly, task '1' needs to finish 
+before '2' can be scheduled. A possible scheduling of tasks is: [0, 1, 2] 
+Example 2:
+
+Input: Tasks=3, Prerequisites=[0, 1], [1, 2], [2, 0]
+Output: []
+Explanation: The tasks have cyclic dependency, therefore they cannot be scheduled.
+Example 3:
+
+Input: Tasks=6, Prerequisites=[2, 5], [0, 5], [0, 4], [1, 4], [3, 2], [1, 3]
+Output: [0 1 4 3 2 5] 
+Explanation: A possible scheduling of tasks is: [0 1 4 3 2 5] 
+
+Time Complexity #
+In step ‘d’, each vertex will become a source only once and each edge will be accessed and removed once. Therefore, the time complexity of the above algorithm will be O(V+E)O(V+E), where ‘V’ is the total number of vertices and ‘E’ is the total number of edges in the graph.
+
+Space Complexity #
+The space complexity will be O(V+E)O(V+E), since we are storing all of the edges for each vertex in an adjacency list.
+'''
+from collections import deque
+
+
+def topological_sort(vertices, edges):
+  sortedOrder = []
+  if vertices <= 0:
+    return sortedOrder
+
+  # a. Initialize the graph
+  inDegree = {i: 0 for i in range(vertices)}  # count of incoming edges
+  graph = {i: [] for i in range(vertices)}  # adjacency list graph
+
+  # b. Build the graph
+  for edge in edges:
+    parent, child = edge[0], edge[1]
+    graph[parent].append(child)  # put the child into it's parent's list
+    inDegree[child] += 1  # increment child's inDegree
+
+  # c. Find all sources i.e., all vertices with 0 in-degrees
+  sources = deque()
+  for key in inDegree:
+    if inDegree[key] == 0:
+      sources.append(key)
+
+  # d. For each source, add it to the sortedOrder and subtract one from all of its children's in-degrees
+  # if a child's in-degree becomes zero, add it to the sources queue
+  while sources:
+    vertex = sources.popleft()
+    sortedOrder.append(vertex)
+    for child in graph[vertex]:  # get the node's children to decrement their in-degrees
+      inDegree[child] -= 1
+      if inDegree[child] == 0:
+        sources.append(child)
+
+  # topological sort is not possible as the graph has a cycle
+  if len(sortedOrder) != vertices:
+    return []
+
+  return sortedOrder
+
+
+def main():
+  print("Topological sort: " +
+        str(topological_sort(4, [[3, 2], [3, 0], [2, 0], [2, 1]])))
+  print("Topological sort: " +
+        str(topological_sort(5, [[4, 2], [4, 3], [2, 0], [2, 1], [3, 1]])))
+  print("Topological sort: " +
+        str(topological_sort(7, [[6, 4], [6, 2], [5, 3], [5, 4], [3, 0], [3, 1], [3, 2], [4, 1]])))
+
+
+main()
