28734fc6a95d7e9d1e52c37a2957ab13d5d9b41c
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 17:36:05 +0530

    Create subsets04.py

---
+++ subsets04.py
@@ -0,0 +1,53 @@
+'''
+Given a set of distinct numbers, find all of its permutations.
+
+Permutation is defined as the re-arranging of the elements of the set. For example, {1, 2, 3} has the following six permutations:
+
+{1, 2, 3}
+{1, 3, 2}
+{2, 1, 3}
+{2, 3, 1}
+{3, 1, 2}
+{3, 2, 1}
+If a set has ‘n’ distinct elements it will have n!n! permutations.
+
+Example 1:
+
+Input: [1,3,5]
+Output: [1,3,5], [1,5,3], [3,1,5], [3,5,1], [5,1,3], [5,3,1]
+Time complexity #
+We know that there are a total of N!N! permutations of a set with ‘N’ numbers. In the algorithm above, we are iterating through all of these permutations with the help of the two ‘for’ loops. In each iteration, we go through all the current permutations to insert a new number in them on line 17 (line 23 for C++ solution). To insert a number into a permutation of size ‘N’ will take O(N),O(N), which makes the overall time complexity of our algorithm O(N*N!)O(N∗N!).
+
+Space complexity #
+All the additional space used by our algorithm is for the result list and the queue to store the intermediate permutations. If you see closely, at any time, we don’t have more than N!N! permutations between the result list and the queue. Therefore the overall space complexity to store N!N! permutations each containing NN elements will be O(N*N!)O(N∗N!).
+'''
+from collections import deque
+
+
+def find_permutations(nums):
+  numsLength = len(nums)
+  result = []
+  permutations = deque()
+  permutations.append([])
+  for currentNumber in nums:
+    # we will take all existing permutations and add the current number to create new permutations
+    n = len(permutations)
+    for _ in range(n):
+      oldPermutation = permutations.popleft()
+      # create a new permutation by adding the current number at every position
+      for j in range(len(oldPermutation)+1):
+        newPermutation = list(oldPermutation)
+        newPermutation.insert(j, currentNumber)
+        if len(newPermutation) == numsLength:
+          result.append(newPermutation)
+        else:
+          permutations.append(newPermutation)
+
+  return result
+
+
+def main():
+  print("Here are all the permutations: " + str(find_permutations([1, 3, 5])))
+
+
+main()
