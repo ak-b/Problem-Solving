8d273212796c41319aa7d79716f6cbecfa11802f
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 16:09:43 +0530

    Update two_pointers02.py

--- two_pointers02.py
+++ two_pointers02.py
@@ -33,6 +33,26 @@ def remove_duplicates(arr):
   return next_non_duplicate
 
 
+def remove_element(arr, key):
+  nextElement = 0  # index of the next element which is not 'key'
+  for i in range(len(arr)):
+    if arr[i] != key:
+      arr[nextElement] = arr[i]
+      nextElement += 1
+
+  return nextElement
+
+
+def main():
+  print("Array new length: " +
+        str(remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3)))
+  print("Array new length: " +
+        str(remove_element([2, 11, 2, 2, 1], 2)))
+
+
+main()
+
+
 def main():
   print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
   print(remove_duplicates([2, 2, 2, 11]))
