fdddb4cbb881e7b5e549d9d2fdfb7236aefde82f
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 16:10:34 +0530

    Update two_pointers02.py

--- two_pointers02.py
+++ two_pointers02.py
@@ -59,3 +59,8 @@ def main():
 
 
 main()
+'''
+Time and Space Complexity: The time complexity of the above algorithm will be O(N)O(N), where ‘N’ is the total number of elements in the given array.
+
+The algorithm runs in constant space O(1)O(1).
+'''
