7ecde2d7e9cf6e588cc6944f65246ec57c6821df
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-05 17:22:51 +0530

    Create tree_bfs06.py

---
+++ tree_bfs06.py
@@ -0,0 +1,56 @@
+'''
+Given a binary tree and a node, find the level order successor of the given node in the tree. 
+The level order successor is the node that appears right after the given node in the level order traversal.
+
+Time complexity #
+The time complexity of the above algorithm is O(N)O(N), where ‘N’ is the total number of nodes in the tree. This is due to the fact that we traverse each node once.
+
+Space complexity #
+The space complexity of the above algorithm will be O(N)O(N) which is required for the queue. Since we can have a maximum of N/2N/2 nodes at any level (this could happen only at the lowest level), therefore we will need O(N)O(N) space to store them in the queue.
+'''
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_successor(root, key):
+  if root is None:
+    return None
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    currentNode = queue.popleft()
+    # insert the children of current node in the queue
+    if currentNode.left:
+      queue.append(currentNode.left)
+    if currentNode.right:
+      queue.append(currentNode.right)
+
+    # break if we have found the key
+    if currentNode.val == key:
+      break
+
+  return queue[0] if queue else None
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  result = find_successor(root, 12)
+  if result:
+    print(result.val)
+  result = find_successor(root, 9)
+  if result:
+    print(result.val)
+
+
+main()
