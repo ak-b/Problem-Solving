c4665bd8cfe31e2a647f98c16356e9867872f413
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-06 10:09:21 +0530

    Create topological_sort06.py

---
+++ topological_sort/topological_sort06.py
@@ -0,0 +1,104 @@
+'''
+There is a dictionary containing words from an alien language for which we don’t know the ordering of the alphabets. Write a method to find the correct order of the alphabets in the alien language. It is given that the input is a valid dictionary and there exists an ordering among its alphabets.
+
+Example 1:
+
+Input: Words: ["ba", "bc", "ac", "cab"]
+Output: bac
+Explanation: Given that the words are sorted lexicographically by the rules of the alien language, so
+from the given words we can conclude the following ordering among its characters:
+ 
+1. From "ba" and "bc", we can conclude that 'a' comes before 'c'.
+2. From "bc" and "ac", we can conclude that 'b' comes before 'a'
+ 
+From the above two points, we can conclude that the correct character order is: "bac"
+Example 2:
+
+Input: Words: ["cab", "aaa", "aab"]
+Output: cab
+Explanation: From the given words we can conclude the following ordering among its characters:
+ 
+1. From "cab" and "aaa", we can conclude that 'c' comes before 'a'.
+2. From "aaa" and "aab", we can conclude that 'a' comes before 'b'
+ 
+From the above two points, we can conclude that the correct character order is: "cab"
+Example 3:
+
+Input: Words: ["ywx", "wz", "xww", "xz", "zyy", "zwz"]
+Output: ywxz
+Explanation: From the given words we can conclude the following ordering among its characters:
+ 
+1. From "ywx" and "wz", we can conclude that 'y' comes before 'w'.
+2. From "wz" and "xww", we can conclude that 'w' comes before 'x'.
+3. From "xww" and "xz", we can conclude that 'w' comes before 'z'
+4. From "xz" and "zyy", we can conclude that 'x' comes before 'z'
+5. From "zyy" and "zwz", we can conclude that 'y' comes before 'w'
+ 
+From the above five points, we can conclude that the correct character order is: "ywxz"
+
+Time complexity # 
+ is O(V+N)O(V+N).
+
+Space complexity #
+The space complexity will be O(V+N)O(V+N), since we are storing all of the rules for each character in an adjacency list.
+'''
+
+from collections import deque
+
+
+def find_order(words):
+  if len(words) == 0:
+    return ""
+
+  # a. Initialize the graph
+  inDegree = {}  # count of incoming edges
+  graph = {}  # adjacency list graph
+  for word in words:
+    for character in word:
+      inDegree[character] = 0
+      graph[character] = []
+
+  # b. Build the graph
+  for i in range(0, len(words)-1):
+    # find ordering of characters from adjacent words
+    w1, w2 = words[i], words[i + 1]
+    for j in range(0, min(len(w1), len(w2))):
+      parent, child = w1[j], w2[j]
+      if parent != child:  # if the two characters are different
+        # put the child into it's parent's list
+        graph[parent].append(child)
+        inDegree[child] += 1  # increment child's inDegree
+        break  # only the first different character between the two words will help us find the order
+
+  # c. Find all sources i.e., all vertices with 0 in-degrees
+  sources = deque()
+  for key in inDegree:
+    if inDegree[key] == 0:
+      sources.append(key)
+
+  # d. For each source, add it to the sortedOrder and subtract one from all of its children's in-degrees
+  # if a child's in-degree becomes zero, add it to the sources queue
+  sortedOrder = []
+  while sources:
+    vertex = sources.popleft()
+    sortedOrder.append(vertex)
+    for child in graph[vertex]:  # get the node's children to decrement their in-degrees
+      inDegree[child] -= 1
+      if inDegree[child] == 0:
+        sources.append(child)
+
+  # if sortedOrder doesn't contain all characters, there is a cyclic dependency between characters, therefore, we
+  # will not be able to find the correct ordering of the characters
+  if len(sortedOrder) != len(inDegree):
+    return ""
+
+  return ''.join(sortedOrder)
+
+
+def main():
+  print("Character order: " + find_order(["ba", "bc", "ac", "cab"]))
+  print("Character order: " + find_order(["cab", "aaa", "aab"]))
+  print("Character order: " + find_order(["ywx", "wz", "xww", "xz", "zyy", "zwz"]))
+
+
+main()
